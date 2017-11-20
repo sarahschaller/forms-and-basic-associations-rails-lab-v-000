8082216db99d2d9259f0b240ded13fed93c005dd
Sarah Schaller <sarcsch@aol.com>
2017-11-20 19:35:49 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,21 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents(contents)
+    contents.each do |content|
+      if content != ""
+        note = Note.find_or_create_by(content: content)
+        self.notes << note
+      end
+    end
+  end
+end
